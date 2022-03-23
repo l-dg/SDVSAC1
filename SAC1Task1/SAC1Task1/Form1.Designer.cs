ff64f87169e39cde8f9f5f5ab81d6becaa423a3d
l-dg <102114462+l-dg@users.noreply.github.com>
2022-03-23 14:36:22 +1100

    Add files via upload

---
+++ SAC1Task1/SAC1Task1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SAC1Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotalValue = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.numPricePaid = new System.Windows.Forms.NumericUpDown();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numPricePaid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(166, 279);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(466, 279);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(91, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "End the quote";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "How old is the textbook? (years)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "How much did you pay for it?";
+            // 
+            // lblTotalValue
+            // 
+            this.lblTotalValue.AutoSize = true;
+            this.lblTotalValue.Location = new System.Drawing.Point(451, 144);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalValue.TabIndex = 6;
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(451, 180);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(0, 13);
+            this.lblValue.TabIndex = 7;
+            // 
+            // numPricePaid
+            // 
+            this.numPricePaid.DecimalPlaces = 2;
+            this.numPricePaid.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numPricePaid.Location = new System.Drawing.Point(172, 219);
+            this.numPricePaid.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numPricePaid.Name = "numPricePaid";
+            this.numPricePaid.Size = new System.Drawing.Size(120, 20);
+            this.numPricePaid.TabIndex = 8;
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(172, 119);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(120, 20);
+            this.numAge.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numAge);
+            this.Controls.Add(this.numPricePaid);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.lblTotalValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPricePaid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotalValue;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.NumericUpDown numPricePaid;
+        private System.Windows.Forms.NumericUpDown numAge;
+    }
+}
+
---
+++ SAC1Task1/SAC1Task1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAC1Task1
+{   
+    public partial class Form1 : Form
+    {
+        // define variable for later use
+        decimal totalValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            // allow for any value to be entered in NumericUpDown
+            numPricePaid.Maximum = decimal.MaxValue;
+            numAge.Maximum = decimal.MaxValue;
+        }
+
+        // function calculates value given price paid and age, returns value as decimal
+        private decimal calculate(decimal purchasePrice, decimal age)
+        {
+            decimal deprecation = Decimal.Multiply(0.2m, Decimal.Multiply(purchasePrice, age));
+            if (deprecation > purchasePrice)
+            {
+                return 0;
+            }
+            else
+            {
+                return purchasePrice - deprecation;
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // extracting values from NumericUpDown
+            decimal age = numAge.Value; 
+            decimal purchasePrice = numPricePaid.Value;
+            // displays value of current item
+            lblValue.Text = "It is worth $" + calculate(purchasePrice, age).ToString();
+            // calculate total by adding new value to old values
+            totalValue += calculate(purchasePrice, age);
+            // display total value of items
+            lblTotalValue.Text = "The collection so far is worth $" + totalValue.ToString();
+            // clear inputs
+            numAge.Value = 0;
+            numPricePaid.Value = 0;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clear inputs
+            numAge.Value = 0;
+            numPricePaid.Value = 0;
+            totalValue = 0;
+            // clear labels displaying current and total values
+            lblValue.Text = string.Empty;
+            lblTotalValue.Text = string.Empty;
+        }
+    }
+}
