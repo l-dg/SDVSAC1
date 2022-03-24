f3d0581ce32ef0a99ab886e15828463e0abb0780
l-dg <102114462+l-dg@users.noreply.github.com>
2022-03-24 12:25:44 +1100

    Add files via upload

---
+++ SAC1Task2/SAC1Task2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SAC1Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvSalesData = new System.Windows.Forms.DataGridView();
+            this.colTextbook = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSubject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSeller = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPurchaser = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPurchasePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSalePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colProfit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotalProfit = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSalesData
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvSalesData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvSalesData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSalesData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colTextbook,
+            this.colSubject,
+            this.colSeller,
+            this.colPurchaser,
+            this.colPurchasePrice,
+            this.colSalePrice,
+            this.colProfit});
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvSalesData.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvSalesData.Location = new System.Drawing.Point(34, 236);
+            this.dgvSalesData.Name = "dgvSalesData";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvSalesData.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvSalesData.Size = new System.Drawing.Size(735, 150);
+            this.dgvSalesData.TabIndex = 0;
+            // 
+            // colTextbook
+            // 
+            this.colTextbook.HeaderText = "Textbook";
+            this.colTextbook.Name = "colTextbook";
+            // 
+            // colSubject
+            // 
+            this.colSubject.HeaderText = "Subject";
+            this.colSubject.Name = "colSubject";
+            // 
+            // colSeller
+            // 
+            this.colSeller.HeaderText = "Seller";
+            this.colSeller.Name = "colSeller";
+            // 
+            // colPurchaser
+            // 
+            this.colPurchaser.HeaderText = "Purchaser";
+            this.colPurchaser.Name = "colPurchaser";
+            // 
+            // colPurchasePrice
+            // 
+            this.colPurchasePrice.HeaderText = "Purchase price";
+            this.colPurchasePrice.Name = "colPurchasePrice";
+            // 
+            // colSalePrice
+            // 
+            this.colSalePrice.HeaderText = "Sale price";
+            this.colSalePrice.Name = "colSalePrice";
+            // 
+            // colProfit
+            // 
+            this.colProfit.HeaderText = "Profit";
+            this.colProfit.Name = "colProfit";
+            // 
+            // lblTotalProfit
+            // 
+            this.lblTotalProfit.AutoSize = true;
+            this.lblTotalProfit.Location = new System.Drawing.Point(96, 198);
+            this.lblTotalProfit.Name = "lblTotalProfit";
+            this.lblTotalProfit.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalProfit.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(295, 185);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(125, 26);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Profits";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTotalProfit);
+            this.Controls.Add(this.dgvSalesData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSalesData;
+        private System.Windows.Forms.Label lblTotalProfit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTextbook;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSubject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSeller;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPurchaser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPurchasePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSalePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProfit;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ SAC1Task2/SAC1Task2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SAC1Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click_1(object sender, EventArgs e)
+        {
+            // opens file dialog for user to select file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // if user selected a file
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(filePath).ToList();
+                decimal total = 0m;
+
+                // 
+                foreach (string line in lines.Skip(1))
+                {
+                    List<string> items = line.Split(',').ToList();
+                    decimal profit;
+                    decimal purchasePrice = decimal.Parse(items[4]);
+                    string salePrice = items[5];
+
+                    if (salePrice == "NA")
+                    {
+                        profit = purchasePrice * -1;
+                    }
+                    else
+                    {
+                        profit = decimal.Parse(salePrice) - purchasePrice;
+                    }
+                    total += profit;
+                    items.Add(profit.ToString());
+                    dgvSalesData.Rows.Add(items.ToArray());
+                }
+                lblTotalProfit.Text = "Total profit is $" + total.ToString();
+                dgvSalesData.Rows.Add();
+                dgvSalesData.Rows[dgvSalesData.Rows.Count - 1].Cells[5].Value = "Total";
+                dgvSalesData.Rows[dgvSalesData.Rows.Count - 1].Cells[6].Value = $"${total.ToString()}";
+            }
+        }
+
+    }
+}
