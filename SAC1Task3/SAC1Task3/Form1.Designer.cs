6261a4f7463b95b55995af5e3d524fab02ca58b1
l-dg <102114462+l-dg@users.noreply.github.com>
2022-03-30 13:49:47 +1100

    Add files via upload

---
+++ SAC1Task3/SAC1Task3/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SAC1Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comFilter = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvSales = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filter by:";
+            // 
+            // comFilter
+            // 
+            this.comFilter.FormattingEnabled = true;
+            this.comFilter.Items.AddRange(new object[] {
+            "Textbook",
+            "Subject",
+            "Rating"});
+            this.comFilter.Location = new System.Drawing.Point(210, 142);
+            this.comFilter.Name = "comFilter";
+            this.comFilter.Size = new System.Drawing.Size(121, 21);
+            this.comFilter.TabIndex = 2;
+            this.comFilter.SelectedIndexChanged += new System.EventHandler(this.comFilter_SelectedIndexChanged);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(442, 143);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Search:";
+            // 
+            // dgvSales
+            // 
+            this.dgvSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSales.Location = new System.Drawing.Point(25, 190);
+            this.dgvSales.Name = "dgvSales";
+            this.dgvSales.Size = new System.Drawing.Size(734, 235);
+            this.dgvSales.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvSales);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.comFilter);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvSales;
+    }
+}
+
---
+++ SAC1Task3/SAC1Task3/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SAC1Task3
+{
+    public partial class Form1 : Form
+    {
+        string filter;
+        List<Sale> sales = new List<Sale> ();
+        BindingSource bs = new BindingSource ();
+        public Form1()
+        {
+            InitializeComponent();
+            // loads and reads csv file then displays it to datagridview
+            LoadCSV();
+            bs.DataSource = sales;
+            dgvSales.DataSource = bs;
+        }
+        private void LoadCSV()
+        {
+            // has user select csv file on launch
+            OpenFileDialog openFileDialog = new OpenFileDialog ();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // reads csv to list "sales"
+                string filePath = openFileDialog.FileName;
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(filePath).ToList();
+                foreach (string line in lines.Skip(1))
+                {
+                    List<string> fields = line.Split(',').ToList();
+                    Sale s = new Sale();
+                    s.textbook = fields[0];
+                    s.subject = fields[1];
+                    s.purchase = decimal.Parse(fields[4]);
+                    s.sale = fields[5];
+                    s.rating = fields[6];
+                    sales.Add(s);
+                }
+            }
+            
+        }
+
+        private void comFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // update variable when filter changes, if filter is rating then run sorting algorithm
+            filter = comFilter.Text;
+            if (filter == "Rating")
+            {
+                List<Sale> fixedList = sales;
+                foreach(var entry in fixedList)
+                {
+                    if(entry.rating == "none")
+                    {
+                        entry.rating = "0";
+                    }
+                }
+                sortByRating(fixedList);
+            }
+            dgvSales.DataSource = bs;
+            bs.ResetBindings(false);
+        }
+        // selection sort algorithm to sort by rating, descending
+        private void sortByRating(List<Sale> _sales)
+        {
+            int minVal;
+            string temp;
+            for (int i = 0; i < _sales.Count - 1; i++)
+            {
+                minVal = i;
+                for (int j = i + 1; j < _sales.Count; j++)
+                {
+
+                    if (int.TryParse(_sales[j].rating, out int ratingJ))
+                    {
+                        if (int.TryParse(_sales[minVal].rating, out int ratingMin))
+                        { if (ratingJ < ratingMin) minVal = j; }
+                        else
+                        {
+                            minVal = j;
+                        }
+                    }
+                }                
+                temp = _sales[minVal].rating;
+                _sales[minVal].rating = _sales[i].rating;
+                _sales[i].rating = temp;
+            }
+        }
+        private List<Sale> Search(string target, string filter)
+        {
+            List<Sale> results = new List<Sale>();
+            // creates list containing only entries containing search prompt
+            foreach(Sale s in sales)
+            {
+                if(filter == "Rating")
+                {
+                    if (s.rating.ToLower() == target.ToLower()) results.Add(s);
+                }
+                if (filter == "Subject")
+                {
+                    if (s.subject.ToLower().Contains(target.ToLower())) { results.Add(s); Console.WriteLine(target); }
+                }
+                if (filter == "Textbook")
+                {
+                    if (s.textbook.ToLower().Contains(target.ToLower())) { results.Add(s); Console.WriteLine(target); }
+                }
+            }
+            return results;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            // whenever searchbox input is changed, searches and displays results
+            List<Sale> r = Search(txtSearch.Text, filter);
+            bs.DataSource = r;
+            dgvSales.DataSource = r;
+            bs.ResetBindings(false);
+        }
+    }
+}
