6efe3dc35d8c67d8efc2c066a23535493d20fd0c
l-dg <102114462+l-dg@users.noreply.github.com>
2022-03-30 14:32:12 +1100

    Add files via upload

---
+++ SAC1Task3/SAC1Task3/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAC1Task3
+{
+    internal class Sale
+    {
+        public string textbook { get; set; }
+        public string subject { get; set; }
+        public decimal purchase { get; set; }
+        public string sale { get; set; }
+        public string rating { get; set; }
+    }
+}
